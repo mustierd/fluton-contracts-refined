9e106e3db4b9a0c1200ff147d807dc0e4ac5625c
cozfuttu <canozfuttu07@gmail.com>
2024-10-21 02:46:44 +0300

    chore: ditched ignition

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -4,11 +4,13 @@ import deploy from "../scripts/private/_deploy";
 const deployTask = task("deploy", "Deploys BridgeTest contract")
   .addPositionalParam("sourceNetwork")
   .setAction(async ({ sourceNetwork }, hre) => {
-    const chainId = hre.config.networks[sourceNetwork].chainId;
+    const chainId = hre.config.networks[sourceNetwork]?.chainId;
 
     if (!chainId) {
       throw new Error("Source network not found");
     }
+
+    await hre.switchNetwork(sourceNetwork);
 
     const address = await deploy({ networkId: chainId }, hre);
 
